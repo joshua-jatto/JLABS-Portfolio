fd0a3c6c136093476ca2d30067e6615e10d3bfdc
joshua <j.j.jatto@gmail.com>
2024-08-23 14:51:20 +0100

    ui fix

--- main.js
+++ main.js
@@ -1,156 +1,141 @@
 //helper functions
-function _(type) { return document.createElement(type) } //call function with html element to create
-function __(type) { return document.querySelector(type) } //call function with html element to get/fetch
+function _(type) {
+  return document.createElement(type);
+} //call function with html element to create
+function __(type) {
+  return document.querySelector(type);
+} //call function with html element to get/fetch
 
+import projectsData from './projectsData.js'
 
-const toggleMode = __('.toggle')
-const body = __('body')
-const quotesBox = __('.quotes')
-const addQuoteBtn = __('.addbtn')
-const getQuote = __('.get-quote')
-const addQnN = __('.add-quote-and-name')
-const nav = __('nav')
-
+const toggleMode = __(".toggle");
+const body = __("body");
+const quotesBox = __(".quotes");
+const addQuoteBtn = __(".addbtn");
+const getQuote = __(".get-quote");
+const addQnN = __(".add-quote-and-name");
+const nav = __("nav");
 
 function darkModeToggle() {
-
-    toggleMode.classList.toggle('active');
-    body.classList.toggle('active')
-
-    console.log('toggled')
+  toggleMode.classList.toggle("active");
+  body.classList.toggle("active");
 }
+toggleMode.addEventListener('click',()=> darkModeToggle())
 
-let quoteMachine = [{
+let quoteMachine = [
+  {
     quote: "I loved you first",
-    quoter: "Jesus IAmthatIAm"
-},
+    quoter: "Jesus IAmthatIAm",
+  },
 
-{
+  {
     quote: "God is always present, He's here now.",
-    quoter: "I. MaryAnne"
-},
+    quoter: "I. MaryAnne",
+  },
 
-{
+  {
     quote: "Only God knows how best to help a man",
-    quoter: "S. Mone"
-},
-{
+    quoter: "S. Mone",
+  },
+  {
     quote: "How be it, It shall come to pass",
-    quoter: "E. Gloria"
-},
-{
+    quoter: "E. Gloria",
+  },
+  {
     quote: "...part of the journey is the end.",
-    quoter: "Tony"
-},
-{
+    quoter: "Tony",
+  },
+  {
     quote: " In the end, it's you and God.",
-    quoter: " J. Rebecca"
-},
-{
+    quoter: " J. Rebecca",
+  },
+  {
     quote: " Insist on winning",
-    quoter: "O. Stephanie "
-},
-{
-    quote: "...comes great power, comes great responsibilty / To whom much is given, much shall be required.",
-    quoter: "SpiderMan / st. Luke 12:48b"
-},
+    quoter: "O. Stephanie ",
+  },
+  {
+    quote:
+      "...comes great power, comes great responsibilty / To whom much is given, much shall be required.",
+    quoter: "SpiderMan / st. Luke 12:48b",
+  },
 ];
 
 let magicNum = Math.floor(Math.random() * quoteMachine.length);
 
 for (let i = magicNum; i <= quoteMachine.length; i++) {
-    quotesBox.innerText = quoteMachine[i].quote + "\n~" + quoteMachine[i].quoter
-    break;
-};
+  quotesBox.innerText = quoteMachine[i].quote + "\n~" + quoteMachine[i].quoter;
+  break;
+}
 
-addQuoteBtn.addEventListener('click', () => {
-    addQuoteBtn.style.display = 'none';
-    getQuote.style.display = 'flex';
-})
+addQuoteBtn.addEventListener("click", () => {
+  addQuoteBtn.style.display = "none";
+  getQuote.style.display = "flex";
+});
 
 //add quote input validation //todo1
-addQnN.addEventListener('click', (e) => {
+addQnN.addEventListener("click", (e) => {
+  function Quote() {
+    return {
+      quote: __("#quote").value,
+      quoter: __("#author").value,
+    };
+  }
 
-    function Quote() {
-        return {
-            quote: __('#quote').value,
-            quoter: __('#author').value
-        }
-    }
+  let newquote = Quote();
+  window.localStorage.setItem("qoutesUpdate", JSON.stringify(newquote));
 
-    let newquote = Quote()
-    window.localStorage.setItem('qoutesUpdate', JSON.stringify(newquote));
+  let myQuote = window.localStorage.getItem("qoutesUpdate");
 
-    let myQuote = window.localStorage.getItem('qoutesUpdate');
+  getQuote.style.display = "none";
+  quoteMachine.push(JSON.parse(myQuote));
+  quotesBox.innerText =
+    JSON.parse(myQuote).quote + "\n~" + JSON.parse(myQuote).quoter;
 
-    getQuote.style.display = 'none'
-    quoteMachine.push(JSON.parse(myQuote));
-    quotesBox.innerText = JSON.parse(myQuote).quote + "\n~" + JSON.parse(myQuote).quoter;
-
-    e.preventDefault()
+  e.preventDefault();
 });
 
 // #1todo fix localstorage to display items on qutoes
 
-
 //implent dynamic loading of projects
 //get parent div
-const projectsContainer = __('.projects-container');
-//create prjects data
-const projectsData = [
-    {
-        id: 1,
-        title: 'Aim Trainer',
-        imageSrc: 'url(./assets/AimTrainer_Desktop.html.png)',
-        description: 'Randomly click the box (shooter game concept)',
-        link: './project-pages/project-1.html',
-    },
-    {
-        id: 2,
-        title: 'Speedy Fabricator',
-        imageSrc: 'url(./assets/Speedy-banner.png)',
-        description: 'Aluminium window maker app for Engineers💪',
-        link: ' https://joshua-jatto.github.io/JLABS-Speedy_ACP/',
+const projectsContainer = __(".projects-container");
 
-    },
-    {
-        id: 3,
-        title: 'Rycipee-finder',
-        imageSrc: 'url(./assets/Rycipee-banner.png)',
-        description: 'Search recipes online😋',
-        link: 'https://joshua-jatto.github.io/Rycipee-finder/',
-
-    },
-    {
-        id: 4,
-        title: 'Coming Soon',
-        imageSrc: 'url(./assets/JTHD.png)',
-        description: 'More project coming soon',
-        link: '/',
-
-    },
-    // {
-    //     id:4, //update id count on next item
-    //     title:'',
-    //     imageSrc:'url(./assets/...)',
-    //     description:'eep it short💪',
-    //     link:'https://joshua-jatto.github.io/',
-
-    // },
-];
 
 //initialize dynamic loading of data to ui
-projectsData.length > 0 ?
-    projectsData.forEach(item => {
-        projectsContainer.innerHTML += `<a href=${item.link} target="_blank" class="project-card">
-    <div class="background" style="background-image:${item.imageSrc}">
-      <div class="project-name">${item.title}</div>
-      <div class="project-description">
-      ${item.description}
-      </div>
-    </div>
-  </a>`
-
-    })
-    : null
-
+projectsData.length > 0
+   ? projectsData.forEach((product) => {
+    // Create a card div
+    const card = document.createElement('div');
+    card.className = 'card';
+  
+    // Create an image element
+    const image = document.createElement('div');
+    image.className = 'card-image';
+    image.style.backgroundImage = product.imageSrc;
+  
+    // Create a title element
+    const title = document.createElement('h2');
+    title.className = 'card-title';
+    title.textContent = product.title;
+  
+    // Create a description element
+    const description = document.createElement('p');
+    description.className = 'card-description';
+    description.textContent = product.description;
+  
+    // Create a link element
+    const link = document.createElement('a');
+    link.target = '_blank'
+    link.className = 'card-link';
+    link.href = product.link;
+    link.textContent = 'Check it out🚀';
+  
+    // Append elements to the card
+    card.appendChild(image);
+    card.appendChild(title);
+    card.appendChild(description);
+    card.appendChild(link);
+  
+    // Append the card to the product container
+    projectsContainer.appendChild(card);
+   }) : null
---
+++ projectsData.js
@@ -0,0 +1,48 @@
+const projectsData = [
+    {
+      id: 1,
+      title: "Aim Trainer",
+      imageSrc: "url(./assets/AimTrainer_Desktop.html.png)",
+      description: "Randomly click the box (shooter game concept)",
+      link: "./project-pages/project-1.html",
+    },
+    {
+      id: 2,
+      title: "Speedy Fabricator",
+      imageSrc: "url(./assets/Speedy-banner.png)",
+      description: "Aluminium window maker app for Engineers💪",
+      link: " https://joshua-jatto.github.io/JLABS-Speedy_ACP/",
+    },
+    {
+      id: 3,
+      title: "Rycipee-finder",
+      imageSrc: "url(./assets/Rycipee-banner.png)",
+      description: "Search recipes online😋",
+      link: "https://joshua-jatto.github.io/Rycipee-finder/",
+    },
+    {
+      id: 4,
+      title: "Emoji Store🏬",
+      imageSrc: "url(./assets/emoJiStore.png)",
+      description: "Reacts⚛ E-commerce store ",
+      link: "https://joshua-jatto.github.io/Emoji-Ecommerce-App/",
+    },
+    {
+      id: 5,
+      title: "Coming Soon",
+      imageSrc: "url(./assets/JTHD.png)",
+      description: "More project coming soon",
+      link: "/",
+    },
+  
+    // {
+    //     id:5, //update id count on next item
+    //     title:'Emoji Store🏬',
+    //     imageSrc:'url(./assets/emohiStore.png)',
+    //     description:'Reacts context E-commerce store',
+    //     link:'https://joshua-jatto.github.io/',
+  
+    // },
+  ];
+
+  export default projectsData
